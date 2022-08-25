1bd49480b31fc88825f5cab08bad192b57da1f62
Andres Lopez <andres@MacBook-Pro-de-Andres.local>
2022-08-25 09:58:34 -0500

    Add Initial commit

---
+++ public/build/root-O42K7S6T.js
@@ -0,0 +1,33 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "/build/_shared/chunk-GPCA4VAL.js";
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-GWJFZCPI.js";
+
+// browser-route-module:root.tsx?browser
+init_react();
+
+// app/root.tsx
+init_react();
+var meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1"
+});
+function App() {
+  return /* @__PURE__ */ React.createElement("html", {
+    lang: "en"
+  }, /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement(Meta, null), /* @__PURE__ */ React.createElement(Links, null)), /* @__PURE__ */ React.createElement("body", null, /* @__PURE__ */ React.createElement(Outlet, null), /* @__PURE__ */ React.createElement(ScrollRestoration, null), /* @__PURE__ */ React.createElement(Scripts, null), /* @__PURE__ */ React.createElement(LiveReload, null)));
+}
+export {
+  App as default,
+  meta
+};
+//# sourceMappingURL=/build/root-O42K7S6T.js.map
